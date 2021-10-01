61adda4700fb053c9187bd446b6c47e8a4b02564
DucQuocXa <duc.ngogia310302@gmail.com>
2021-10-01 11:48:56 +0300

    Posts card

--- Client/my-app/src/Components/Posts/Post/Post.js
+++ Client/my-app/src/Components/Posts/Post/Post.js
@@ -1,10 +1,34 @@
 import React from "react";
 import useStyles from "./style";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@mui/material";
+import ThumbUpAltIcon from "@mui/icons-material/ThumbUpAlt";
+import DeleteIcon from "@mui/icons-material/Delete";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import moment from "moment";
 
-function Post() {
+function Post({ post }) {
   const classes = useStyles();
 
-  return <div></div>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{post.message}</Typography>
+      </div>
+    </Card>
+  );
 }
 
 export default Post;
--- Client/my-app/src/Components/Posts/Posts.js
+++ Client/my-app/src/Components/Posts/Posts.js
@@ -1,16 +1,24 @@
 import React from "react";
-//import useStyles from "./style";
+import useStyles from "./style";
+import { Grid, CircularProgess } from "@mui/material";
 import { useSelector } from "react-redux";
+import Post from "./Post/Post";
 
 function Posts() {
   const posts = useSelector((state) => state.posts);
-  //const classes = useStyles();
+  const classes = useStyles();
 
   console.log(posts);
-  return (
-    <div>
-      <h2>Posts</h2>
-    </div>
+  return !posts.length ? (
+    <CircularProgess />
+  ) : (
+    <Grid className={classes.mainContainer} alignItems="stretch" spacing={3}>
+      {posts.map((post) => {
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>;
+      })}
+    </Grid>
   );
 }
 
