5b1e6912dc3631542f6588eb520283a4133f5631
DucQuocXa <duc.ngogia310302@gmail.com>
2021-11-04 01:55:19 +0200

    create auth middleware to check users

--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,4 +1,5 @@
 import express, { Router } from "express";
+
 import {
   getPost,
   createPost,
@@ -6,13 +7,14 @@ import {
   deletePost,
   likePost,
 } from "../controllers/postsFunction.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.get("/", getPost);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/likePost", likePost);
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
 
 export default router;
